deaabe7459ee813d3811bf9cb2ca50fe30e39aed
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-09 00:11:18 +0000

    testing

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("s", {
+addLayer("p", {
     name: "shadow", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
