d3a6ae5753ce57eb127ad7b20e735694c6ded42b
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-09 13:37:57 +0000

    t

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+        upgrades: {
+        11: {
+    title: "Dark",
+    description: "Double your point gain.",
+    cost: new Decimal(1),
+        },
+    },
 })
