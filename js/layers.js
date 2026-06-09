2a0eff5d89d001c00ec2a6cc46d24c4d69dd829a
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-09 20:58:45 +0000

    fix

--- js/layers.js
+++ js/layers.js
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "#595959",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Shadows", // Name of prestige currency
-    baseResource: "Shades", // Name of resource prestige is based on
+    resource: "shadows", // Name of prestige currency
+    baseResource: "shades", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
