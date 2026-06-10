da2f72b200afe6a0e8c33af92cb7eaa565722f02
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 23:44:36 +0000

    eda

--- js/layers.js
+++ js/layers.js
@@ -24,6 +24,8 @@ addLayer("s", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
         upgrades: {
+            rows: 2,
+            cols: 3,
         11: {
     title: "Dark",
     description: "Doubles your shades gain.",
