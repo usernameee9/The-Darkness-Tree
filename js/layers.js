6b8f53bf483e2cd7654c59744bbfa34a3ef6c63c
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 17:00:13 +0000

    s

--- js/layers.js
+++ js/layers.js
@@ -57,7 +57,6 @@ addLayer("s", {
         return player.d.points.add(1).pow(0.5)
     },
     effectDisplay() { return format(upgradeEffect(d, this.id))+"x" }, // Add formatting to the effect 
-   unlocked(){false}
     },
         }
     }
