bdefb236079202b584b270dcd5cf5d28e285d803
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 17:57:19 +0000

    e

--- js/layers.js
+++ js/layers.js
@@ -52,12 +52,12 @@ addLayer("s", {
     14: {
     title: "Darkest",
     description: "Darkness increases shades gain",
-    cost: new Decimal(10),
+    cost: new Decimal(30),
          effect() {
         return player.d.points.add(1).pow(0.5)
     },
     effectDisplay() { return format(upgradeEffect(d, this.id))+"x" }, // Add formatting to the effect
-unlocked() { return hasMilestone(d, 0) }
+unlocked() { return hasMilestone('d', 0) }
     },
     }
 }
