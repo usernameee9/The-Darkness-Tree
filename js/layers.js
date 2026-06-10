18108853b479a66c176249eaafe4ecf4fee429c8
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 17:01:56 +0000

    e

--- js/layers.js
+++ js/layers.js
@@ -49,17 +49,8 @@ addLayer("s", {
     effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect 
   
     },
-    14: {
-    title: "Darkest",
-    description: "Darkness increases shades",
-    cost: new Decimal(10),
-         effect() {
-        return player.d.points.add(1).pow(0.5)
-    },
-    effectDisplay() { return format(upgradeEffect(d, this.id))+"x" }, // Add formatting to the effect 
-    },
-        }
     }
+}
 ),
 addLayer("d", {
     startData() { return {                  // startData is a function that returns default data for a layer. 
