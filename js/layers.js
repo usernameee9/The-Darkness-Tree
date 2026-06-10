d41103f69d0688f6726bb7a76907c8b49b92505c
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 14:33:13 +0000

    idk

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,7 @@ addLayer("s", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('d', 12)) mult = mult.times(2)
+        if (hasUpgrade('d',12)) mult = mult.times(2)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
