3fcac35fdb1a81bcac176ef181d85a597f006944
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 00:23:31 +0000

    d

--- js/mod.js
+++ js/mod.js
@@ -44,6 +44,7 @@ function getPointGen() {
 	if (hasUpgrade('s', 11)) gain = gain.times(2)
     if (hasUpgrade('s', 12)) gain = gain.times(upgradeEffect('s', 12))
 	if (hasUpgrade('s', 13)) gain = gain.times(upgradeEffect('s', 13))
+	if (hasUpgrade('d', 11)) gain = gain.times(2)
 	return gain
 }
 
