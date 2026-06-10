58e986a34089b21024654653a3b5e81eeb5b2d43
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-10 16:25:59 +0000

    yep pers

--- js/layers.js
+++ js/layers.js
@@ -58,7 +58,7 @@ addLayer("s", {
     },
     effectDisplay() { return format(upgradeEffect(d, this.id))+"x" }, // Add formatting to the effect 
   unlocked(){false},
-  hasMilestone(){d, 11},unlocked(){true},
+  hasMilestone(){d,11},unlocked(){true},
     },
         }
     }
