89173121f510ffdb33840b79b4a837c6044f9416
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-11 21:59:37 +0000

    e

--- js/layers.js
+++ js/layers.js
@@ -24,6 +24,8 @@ addLayer("s", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
         upgrades: {
+            rows: 1,
+            cols: 4,
         11: {
     title: "Dark",
     description: "Doubles your shades gain.",
