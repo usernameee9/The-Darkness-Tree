2eb97d853a1867f0a08c9a667fa236e0764a1e94
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-12 20:30:03 +0000

    e

--- js/layers.js
+++ js/layers.js
@@ -55,7 +55,7 @@ addLayer("s", {
     description: "Darkness increase shades gain",
     cost: new Decimal(30),
     unlocked() {
-        return hasUpgrade('darkness',13)
+        return hasUpgrade('d',13)
     },
          effect() {
         return player.d.points.add(1).pow(0.5)
