bb3571096a867b6bcf5db07c58ee3b955179b5fb
usernameee9 <skuzick4914@hwdsb.on.ca>
2026-06-18 21:29:38 +0000

    e

--- js/layers.js
+++ js/layers.js
@@ -28,11 +28,22 @@ addLayer("s", {
         body() { return "You decided that this world is filled with too much light so you decided to purge it of the light and you must start with the very beginning." },
     },
 },
+buyables: {
+    11: {
+        cost(x) { return new Decimal(100).mul(x) },
+        display() { return "Blah" },
+        canAfford() { return player[this.layer].points.gte(this.cost()) },
+        buy() {
+            player[this.layer].points = player[this.layer].points.sub(this.cost())
+            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+        },
+    },
+},
     row: 0, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
         upgrades: {
             rows: 2,
-            cols: 4,
+            cols: 5,
         11: {
     title: "Dark",
     description: "Doubles your shades gain.",
@@ -61,6 +72,11 @@ addLayer("s", {
     title: "Triple the trouble",
     description: "Triples your shades and shadows gain.",
     cost: new Decimal(30),
+        },
+                       15: {
+    title: "New features",
+    description: "Unlocks a buyable.",
+    cost: new Decimal(50),
         },
     },
 }
